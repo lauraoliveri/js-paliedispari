7dd427c76697960295b8a01e94771628c44b322d
lauraoliveri <lauraoliveri2003@gmail.com>
2024-07-22 00:42:51 +0200

    Add input radio in html and edit js file.  Make "Vai" button responsive with js

--- Pari e dispari/js/scripts.js
+++ Pari e dispari/js/scripts.js
@@ -20,15 +20,15 @@ const Somma = (Random() + UserNumber);
 function IsEven (num) {
 
     if (IsEven % 2 ==0) {
-        alert('Hai vinto!')
+        return true;
     }
     else {
-        alert('Hai perso!');
+        return false;
     }
 }
 
 
-// bottone vai
+// bottone per il risultato finale
 
 const BottoneVai = document.getElementById('btn_vai'); 
 
@@ -39,7 +39,28 @@ BottoneVai.addEventListener('click', function() {
     // somma dei numeri
     console.log('La somma dei due numeri è',Somma);
 
-    if (Pari.checked && boh) {
+    // se l'user ha scelto pari allora
+    if (Pari.checked) {
+        
+        if (IsEven(Somma) === true) {
+            alert('Hai vinto!')
+        }
+        else {
+            alert('Hai perso!')
+        }
+
+    }
+
+    // se l'user ha scelto dispari allora
+    
+    else if (Dispari.checked) {
+
+        if (IsEven(Somma) === false) {
+            alert('Hai vinto!')
+        }
+        else {
+            alert('Hai perso!')
+        }
 
     }
 })
